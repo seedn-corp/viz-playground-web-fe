44a27ba11b976c39f0d3d891cd3c7583091bb8b0
Danheon <eksgjs3847@gmail.com>
2025-09-02 17:37:34 +0900

    feat: 파일 드래그앤드롭 업로드 기능 추가

--- src/components/chart/FileUploader/styles.ts
+++ src/components/chart/FileUploader/styles.ts
@@ -1,16 +1,29 @@
 import { css, type Theme } from '@emotion/react';
 
-export const sideBarCss = {
-  sampleDownloadButton: (theme: Theme) =>
+export const fileUploaderCss = {
+  fileInput: css({
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: 110,
+    overflow: 'hidden',
+    opacity: 0,
+  }),
+
+  fileUploadButton: (theme: Theme) =>
     css({
+      position: 'relative',
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      gap: 8,
       border: `1px solid ${theme.colors.gray_050}`,
       borderRadius: 5,
       width: '100%',
-      height: 30,
+      height: 110,
       transition: 'all 200ms',
 
       ':hover': {
@@ -21,4 +34,9 @@ export const sideBarCss = {
         backgroundColor: theme.colors.gray_040,
       },
     }),
+
+  uploadIcon: css({
+    width: 14,
+    height: 14,
+  }),
 };

ac47a7784b7bbeef03cdf95e014ecb1b34b6c6ec
Danheon <eksgjs3847@gmail.com>
2025-09-03 12:43:06 +0900

    chore: type 추가

---
+++ src/types/widget/index.ts
@@ -0,0 +1,53 @@
+export type WidgetType =
+  | 'bar_chart'
+  | 'line_chart'
+  | 'pie_chart'
+  | 'table'
+  | 'metric'
+  | 'scatter_plot';
+
+export type WidgetDetailResponse = {
+  id: string;
+  dashboard_id: string;
+
+  name: string;
+  type: WidgetType;
+  processed_data: string;
+  position: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+  config: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type CreateWidgetParams = {
+  name: string;
+  type: WidgetType;
+  processed_data: string;
+  position: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+  config?: string;
+};
+
+export type UpdateWidgetParams = {
+  id: number;
+
+  name?: string;
+  type?: string;
+  processed_data?: string;
+  position?: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+  config?: string;
+};

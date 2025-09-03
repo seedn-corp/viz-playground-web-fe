4ba0de13a429c45498f18a09a7eb2f8cfd8b7c4d
im-binary <pongpong0121@kakao.com>
2025-09-03 16:09:13 +0900

    fix: API url 수정 및 타입 수정

--- src/apis/widgets/index.ts
+++ src/apis/widgets/index.ts
@@ -1,4 +1,5 @@
 import type { CreateWidgetParams, UpdateWidgetParams, WidgetDetailResponse } from '@/types/widgets';
+
 import client from '../client';
 
 export const getWidgetDetail = async (id: string): Promise<WidgetDetailResponse> => {
@@ -7,11 +8,17 @@ export const getWidgetDetail = async (id: string): Promise<WidgetDetailResponse>
 };
 
 export const createWidget = async ({ dashboardId, ...params }: CreateWidgetParams) => {
-  const { data } = await client.post(`/api/dashboard/${dashboardId}/widgets`, params);
+  const { data } = await client.post(`/api/dashboards/${dashboardId}/widgets`, params);
   return data;
 };
 
 export const updateWidget = async ({ id, ...params }: UpdateWidgetParams) => {
-  const { data } = await client.put(`/api/dashboard/${id}/widgets`, params);
+  const { data } = await client.put(`/api/widgets/${id}`, params);
   return data;
+};
+
+export const getAllWidget = async (dashboardId: string) => {
+  const { data } = await client.get(`/api/dashboards/${dashboardId}/widgets`);
+
+  return data.widgets;
 };

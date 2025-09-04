8d4f9454fb9d6d96eb486fadab2eef35db20fdf8
윤지 <85012406+yoonhihi97@users.noreply.github.com>
2025-09-04 10:40:53 +0900

    style: Header UI 수정

--- src/components/common/Header/styles.ts
+++ src/components/common/Header/styles.ts
@@ -1,17 +1,20 @@
-import { theme } from "@basiln/design-system";
-import { css } from "@emotion/react";
+import { theme } from '@basiln/design-system';
+import { css } from '@emotion/react';
+
+import { Z_INDEX } from '@/constants/zIndex';
 
 export const headerCss = {
   container: css({
-    position: "fixed",
+    position: 'fixed',
     top: 0,
-    width: "100%",
+    width: '100%',
     minHeight: 40,
-    padding: "10px 20px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
+    padding: '10px 20px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     borderBottom: `1px solid ${theme.colors.gray_050}`,
     backgroundColor: theme.colors.white,
+    zIndex: Z_INDEX.HEADER,
   }),
 };
